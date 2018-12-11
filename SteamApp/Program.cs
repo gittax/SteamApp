f83b9210a2856d12ac0bde462b688e0cd8a615f8
abelozerov <abelozerov@gmcs.ru>
2018-12-11 20:43:07 +0300

    Добавьте файлы проекта.

---
+++ SteamApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteamApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
